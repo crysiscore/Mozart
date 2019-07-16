75220b97b6ab80c2e0a574875cf48dcb43fa6e93
Agnaldo Samuel <agnaldosamuel3@gmail.com>
2019-07-16 17:32:17 +0200

    new scripts

--- scripts/cs_mangungumete.sql
+++ scripts/cs_mangungumete.sql
@@ -2,20 +2,21 @@ use openmrs;
 
 set @ydata := '2019-03-20';
 set @sismaLocationID := 1080610; -- mangungumete
--- set @openmrsLocationID := 410 ;
+set @openmrsID :=212;
+
 
 insert into global_property (property,property_value,description,uuid)
- values('esaudemetadata.hfc',@sismaLocationID,'health facility code',uuid());
- insert into global_property (property,property_value,description,uuid) 
- values('esaudemetadata.dateToImportTo',@ydata,'Date when data should be fetched to provide it',uuid());
+values('esaudemetadata.hfc',@sismaLocationID,'health facility code',uuid());
+insert into global_property (property,property_value,description,uuid) 
+values('esaudemetadata.dateToImportTo',@ydata,'Date when data should be fetched to provide it',uuid());
 
-UPDATE obs en SET en.location_id = @sismaLocationID WHERE en.location_id IS NULL OR en.location_id != @sismaLocationID;
-UPDATE encounter en SET en.location_id = @sismaLocationID WHERE en.location_id IS NULL OR en.location_id != @sismaLocationID;
-UPDATE visit en SET en.location_id = @sismaLocationID WHERE en.location_id IS NULL OR en.location_id != @sismaLocationID;
-UPDATE patient_program en SET en.location_id = @sismaLocationID WHERE en.location_id IS NULL OR en.location_id != @sismaLocationID;
+SET foreign_key_checks = 0;
+UPDATE obs en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
+UPDATE encounter en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
+UPDATE visit en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
+UPDATE patient_program en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
 
 call temp.proc_remove_dups_filas();
-
 call temp.proc_remove_dups_buscas();
 
 -- Check if two patients are using the same NID (Problem caused by synchronization)
@@ -27,4 +28,3 @@ GROUP BY identifier
 HAVING Count(*)>=2;
 
 call export_db.EXPORTDB();
-
