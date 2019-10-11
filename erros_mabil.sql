b2ad01b4f5d68e7c321113fe6a94e662b2138ad8
Agnaldo Samuel <agnaldosamuel3@gmail.com>
2019-10-11 10:12:03 +0200

    update erros cs_mabil

--- erros_mabil.sql
+++ erros_mabil.sql
@@ -3,4 +3,5 @@ SELECT obs_id,person_id, concept_id,order_id,obs_datetime,location_id ,voided
  FROM openmrs.obs WHERE encounter_id= 19227 ORDER BY concept_id ;
 
 
- DELETE FROM openmrs.obs WHERE obs_id IN (241321);
+ -- DELETE FROM openmrs.obs WHERE obs_id IN (241321); 
+  DELETE FROM openmrs.obs WHERE obs_id IN (255745); 
