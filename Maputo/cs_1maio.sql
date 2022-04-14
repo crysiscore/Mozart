46fd13e74becb016997f14a9894d09e7ca19277b
Agnaldo <agnaldosamuel3@mail.com>
2022-04-14 13:38:07 +0200

    1_maio

---
+++ Maputo/cs_1maio.sql
@@ -0,0 +1,44 @@
+/*
+
+mysql -uroot  -ppassword  < MOZART_EXPORTDBUpdateQ22022.sql ;
+mysql -uroot  -ppassword  < sp_temp.sql
+
+
+mysql -uroot  -ppassword -e " grant all privileges on *.* to 'esaude'@'%';  flush privileges; "
+
+mysql -uesaude   -pesaude openmrs
+
+
+*/
+
+SELECT  l.location_id, l.UUID, l.NAME FROM location l WHERE l.NAME in  (SELECT property_value FROM global_property WHERE property = 'default_location' );
+
+
+UPDATE global_property SET property_value = 208 WHERE property='mozart.location';
+
+-- Confirmar o sisma code
+
+select location_id, value_reference  from location_attribute where location_id=208; 
+
+
+
+use openmrs;
+set @openmrsID :=208;
+
+
+
+SET foreign_key_checks = 0;
+UPDATE obs en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
+UPDATE encounter en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
+UPDATE visit en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
+UPDATE patient_program en SET en.location_id = @openmrsID WHERE en.location_id IS NULL OR en.location_id != @openmrsID;
+
+call temp.proc_remove_dups_filas();
+call temp.proc_remove_dups_buscas();
+
+
+
+CALL export_db.EXPORTDB('2022-03-20'); 
+
+
+
--- Maputo/cs_bagamoio.sql
+++ Maputo/cs_bagamoio.sql
@@ -1,57 +1,30 @@
-/* drop database openmrs; create database openmrs; drop database export_db;
+/*
 
-cd /data/Mozart/Q3_2021
-mysql -uroot  -ppassword -e "drop database if exists export_db ; "
-mysql -uroot  -ppassword -e "drop database if exists temp; create database temp;"
+mysql -uroot  -ppassword  < MOZART_EXPORTDBUpdateQ22022.sql ;
+mysql -uroot  -ppassword  < sp_temp.sql
 
-mysql -uroot  -ppassword  < MOZART_EXPORTDUpdate11092020.sql;
-mysql -uroot  -ppassword temp < sp_temp.sql
-mysql -uroot  -ppassword  <  fill_hdd/fill_hhd_bagamoio.sql
 
 mysql -uroot  -ppassword -e " grant all privileges on *.* to 'esaude'@'%';  flush privileges; "
 
-mysql -uroot  -ppassword
+mysql -uesaude   -pesaude
 
 
-SELECT location_id,NAME,address2,county_district,state_province,location_id FROM openmrs.location l WHERE county_district='Maputo Cidade'; 
-SELECT location_id,NAME,address2,county_district,state_province,location_id FROM openmrs.location where name  like '%bagamo%';
 */
 
 SELECT  l.location_id, l.UUID, l.NAME FROM location l WHERE l.NAME in  (SELECT property_value FROM global_property WHERE property = 'default_location' );
 
+
 UPDATE global_property SET property_value = 212 WHERE property='mozart.location';
 
+-- Confirmar o sisma code
 
+select location_id, value_reference  from location_attribute where location_id=212; 
 
 
 
 use openmrs;
 set @openmrsID :=212;
 
-/*
-set @ydata := '2021-12-20';
-set @sismaLocationID := 1110507;  -- cs_bagamoio
-
-
-
-
--- insert into global_property (property,property_value,description,uuid)
---  values('esaudemetadata.hfc',@sismaLocationID,'health facility code',uuid());
--- insert into global_property (property,property_value,description,uuid) 
--- values('esaudemetadata.dateToImportTo',@ydata,'Date when data should be fetched to provide it',uuid());
-
-
--- update global_property 
--- set property_value = @sismaLocationID
--- where  property ='esaudemetadata.hfc';
-
-
--- update global_property 
--- set property_value = @ydata 
--- where  property ='esaudemetadata.dateToImportTo';
-
--- select  property,property_value,description,uuid from global_property where property ='esaudemetadata.hfc' or property='esaudemetadata.dateToImportTo';
-*/
 
 
 SET foreign_key_checks = 0;
@@ -65,7 +38,7 @@ call temp.proc_remove_dups_buscas();
 
 
 
-CALL export_db.EXPORTDB('2021-12-20', 'Maputo Cidade'); 
+CALL export_db.EXPORTDB('2022-03-20'); 
 
 
 
